a2ecbd12c6d06e8b2a5ea65b981bd803cc4311e4
vsaxena33 <55374290+vsaxena33@users.noreply.github.com>
2025-09-11 10:33:54 +0530

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,6 @@
+client_id = "XXXXXXXXXX"                        # App ID
+secret_key = "XXXXXXXXXX"                       # App secret key
+redirect_uri = "https://www.xyz.com/"           # Redirect URL
+user_name = "XXXXXXX"                           # User name
+totp_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"   # TOTP key
+pin = "1234"                                    # Fyers pin
